efcf902b13cbfae2da56033643936d9c295140c4
imnxtamess <davide.morisco.work@gmail.com>
2025-06-25 16:19:13 +0200

    add store and create controllers (need testing)

--- src/main/java/org/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,10 +7,17 @@ import org.java.spring.spring_la_mia_pizzeria_crud.repositories.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -49,4 +56,24 @@ public class PizzaController {
 
     return "pizzas/index";
   }
+
+  @GetMapping("/create")
+  public String create(Model model) {
+
+    return "/pizzas/create";
+  }
+
+  @PostMapping("/create")
+  public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+    if (bindingResult.hasErrors()) {
+
+      return "/pizzas/create";
+    }
+
+    repository.save(formPizza);
+
+    return "redirect:/pizzas/index";
+  }
+
 }
