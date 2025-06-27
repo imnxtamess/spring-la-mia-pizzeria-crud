f4aecbcea854bae141a691f6505e431633f5bbc5
imnxtamess <davide.morisco.work@gmail.com>
2025-06-27 11:19:35 +0200

    add delete modal with javascript and th:data

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,15 +1,40 @@
 const deleteBtns = document.querySelectorAll(".deleteBtn")
-const deleteModal = document.getElementById("deleteModal")
-const saveBtn = document.getElementById("saveBtn")
-
+const deleteModal = document.querySelector(".deleteModal")
+const closeBtn = document.getElementById("closeBtn")
+const confirmMsg = document.getElementById("confirmMsg")
 
 deleteBtns.forEach(deleteBtn => {
   deleteBtn.addEventListener("click", () => {
 
     console.log("clicked")
 
+    const id = deleteBtn.dataset.pizzaid;
+
+
+    const pizzaName = deleteBtn.dataset.pizzaname;
+
+    console.log(pizzaName)
+
+    const deleteForm = document.querySelector(".deleteForm")
+
+    deleteForm.action = `/pizzas/delete/${id}`
+
+    confirmMsg.innerHTML = `Are you sure you want to delete: ${pizzaName} Pizza?`
 
     deleteModal.classList.remove("d-none")
 
+
+
   })
 })
+
+
+closeBtn.addEventListener("click", () => {
+
+  console.log("clicked")
+  console.log(deleteModal.classList)
+  deleteModal.classList.add("d-none")
+  console.log(deleteModal.classList)
+
+})
+
